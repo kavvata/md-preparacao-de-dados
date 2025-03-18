876733c35b26fdfd6cf60299b7a5194dd69f2cae
kavvata <gabriel.kawata@gmail.com>
2025-03-18 15:16:09 -0300

    feat: implementação de limpeza de dados BI-RADS

--- main.py
+++ main.py
@@ -18,3 +18,16 @@ dirty = df_mamo.query(
 )
 
 print(dirty)
+# %% tratando dados faltantes em BI-RADS utilizando moda
+filtro = df_mamo["bi_rads"] == "?"
+moda_bi_rads = st.mode(
+    df_mamo[df_mamo["bi_rads"] != "?"].bi_rads
+)
+
+print(f"moda bi_rads: {moda_bi_rads}")
+
+print(df_mamo[filtro].bi_rads)
+
+df_tratado.loc[filtro, 'bi_rads'] = int(moda_bi_rads) + df_tratado[filtro].severity
+
+print(df_tratado[filtro])
