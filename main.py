d448cb4094d68c15a3a4913f08c20fde1da2fdf7
kavvata <gabriel.kawata@gmail.com>
2025-03-18 18:04:50 -0300

    refactor: formatação

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ df_tratado = df_mamo.copy()
 print(df_mamo)
 
 # %% verificando quantos objetos tem dados faltando
-print(df_mamo.query(
-    'bi_rads == "?"'
-    + 'or age == "?"'
-    + 'or shape == "?"'
-    + 'or margin == "?"'
-    + 'or density == "?"'
-    + 'or severity == "?"'
-))
+print(
+    df_mamo.query(
+        'bi_rads == "?"'
+        + 'or age == "?"'
+        + 'or shape == "?"'
+        + 'or margin == "?"'
+        + 'or density == "?"'
+        + 'or severity == "?"'
+    )
+)
 # %% tratando dados faltantes em BI-RADS utilizando moda
 filtro = df_mamo["bi_rads"] == "?"
 moda_bi_rads = st.mode(df_mamo[df_mamo["bi_rads"] != "?"].bi_rads)
@@ -24,7 +26,7 @@ print(f"moda bi_rads: {moda_bi_rads}")
 
 print(df_mamo[filtro].bi_rads)
 
-df_tratado.loc[filtro, 'bi_rads'] = int(moda_bi_rads) + df_tratado[filtro].severity
+df_tratado.loc[filtro, "bi_rads"] = int(moda_bi_rads) + df_tratado[filtro].severity
 
 print(df_tratado[filtro])
 # %% tratando dados faltantes em idade
@@ -35,38 +37,58 @@ print(f"media idade: {media_idade}")
 filtro = df_mamo["age"] == "?"
 print(df_mamo[filtro])
 
-df_tratado.loc[filtro, 'age'] = media_idade
+df_tratado.loc[filtro, "age"] = media_idade
 
 print(df_tratado[filtro])
 # %% tratando de dados faltantes em forma
-moda_forma_benigna = st.mode([int(shape) for shape in df_mamo.loc[
-    (df_mamo["severity"] == 0) & (df_mamo["shape"] != "?"), "shape"
-]])
+moda_forma_benigna = st.mode(
+    [
+        int(shape)
+        for shape in df_mamo.loc[
+            (df_mamo["severity"] == 0) & (df_mamo["shape"] != "?"), "shape"
+        ]
+    ]
+)
 
-moda_forma_maligna = st.mode([int(shape) for shape in df_mamo.loc[
-    (df_mamo["severity"] == 1) & (df_mamo["shape"] != "?"), "shape"
-]])
+moda_forma_maligna = st.mode(
+    [
+        int(shape)
+        for shape in df_mamo.loc[
+            (df_mamo["severity"] == 1) & (df_mamo["shape"] != "?"), "shape"
+        ]
+    ]
+)
 
 
 print(f"media forma severidade benigna : {moda_forma_benigna}")
 print(f"media forma severidade maligna : {moda_forma_maligna}")
 
-filtro = (df_mamo["shape"] == "?")
+filtro = df_mamo["shape"] == "?"
 print(df_mamo[filtro].head())
 
-df_tratado.loc[filtro & (df_mamo["severity"] == 0), 'shape'] = moda_forma_benigna
-df_tratado.loc[filtro & (df_mamo["severity"] == 1), 'shape'] = moda_forma_maligna
+df_tratado.loc[filtro & (df_mamo["severity"] == 0), "shape"] = moda_forma_benigna
+df_tratado.loc[filtro & (df_mamo["severity"] == 1), "shape"] = moda_forma_maligna
 
 print(df_tratado[filtro].head())
 
 # %% tratando de dados faltantes em contorno
-moda_contorno_benigna = st.mode([int(margin) for margin in df_mamo.loc[
-    (df_mamo["severity"] == 0) & (df_mamo["margin"] != "?"), "margin"
-]])
+moda_contorno_benigna = st.mode(
+    [
+        int(margin)
+        for margin in df_mamo.loc[
+            (df_mamo["severity"] == 0) & (df_mamo["margin"] != "?"), "margin"
+        ]
+    ]
+)
 
-moda_contorno_maligna = st.mode([int(margin) for margin in df_mamo.loc[
-    (df_mamo["severity"] == 1) & (df_mamo["margin"] != "?"), "margin"
-]])
+moda_contorno_maligna = st.mode(
+    [
+        int(margin)
+        for margin in df_mamo.loc[
+            (df_mamo["severity"] == 1) & (df_mamo["margin"] != "?"), "margin"
+        ]
+    ]
+)
 
 print(f"media contorno severidade benigna : {moda_contorno_benigna}")
 print(f"media contorno severidade maligna : {moda_contorno_maligna}")
@@ -84,12 +106,22 @@ print(df_tratado[filtro].head(6))
 
 # NOTE: no material fornecido pelo livro, a densidade é a mesma para as duas classes,
 # sendo possivel eliminar um calculo de moda
-moda_densidade_benigna = st.mode([int(density) for density in df_mamo.loc[
-    (df_mamo["severity"] == 0) & (df_mamo["density"] != "?"), "density"
-]])
-moda_densidade_maligna = st.mode([int(density) for density in df_mamo.loc[
-    (df_mamo["severity"] == 1) & (df_mamo["density"] != "?"), "density"
-]])
+moda_densidade_benigna = st.mode(
+    [
+        int(density)
+        for density in df_mamo.loc[
+            (df_mamo["severity"] == 0) & (df_mamo["density"] != "?"), "density"
+        ]
+    ]
+)
+moda_densidade_maligna = st.mode(
+    [
+        int(density)
+        for density in df_mamo.loc[
+            (df_mamo["severity"] == 1) & (df_mamo["density"] != "?"), "density"
+        ]
+    ]
+)
 
 print(f"media densidade severidade benigna : {moda_densidade_benigna}")
 print(f"media densidade severidade maligna : {moda_densidade_maligna}")
